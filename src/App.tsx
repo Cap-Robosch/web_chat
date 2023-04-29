458e25a2afaa9f10d72c9b07629ed8acd8859ff9
viniciusventura29 <viniciusventura29@gmail.com>
2023-04-28 21:51:09 -0300

    improve responsiviness

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,16 @@ function App() {
     }).then((e)=>{
       console.log(e)
       setRoboschAnswer(e.data)
+      setInputMessage("")
     })
   }
 
   return (
     <div className='flex flex-col gap-5 px-10 items-center justify-center h-screen w-screen'>
-      <p className="w-96">{roboschAnswer===""? "Faça uma pergunta":roboschAnswer}</p>
+      <p className="w-80">{roboschAnswer===""? "Faça uma pergunta":roboschAnswer}</p>
       <div className="flex gap-2">
-      <input className="p-2 bg-gray-200 outline-none rounded w-96" value={inputMessage} onChange={(e)=>setInputMessage(e.target.value)} type="text" />
-      <button onClick={sendMessage} className="p-2 rounded bg-gray-200 w-24">Enviar</button>
+      <input className="p-2 bg-gray-200 outline-none rounded w-40 lg:w-96" value={inputMessage} onChange={(e)=>setInputMessage(e.target.value)} type="text" />
+      <button onClick={sendMessage} className="p-2 rounded bg-gray-200 w-16 lg:w-24">Enviar</button>
       </div>
     </div>
   )
