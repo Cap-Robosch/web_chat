24f488a654a3817a5779f889c6ea7a9b23019a46
viniciusventura29 <viniciusventura29@gmail.com>
2023-05-03 16:56:13 -0300

    new component Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = ()=>{
+    return(
+        <div className="bg-white h-full w-72 fixed left-0 pt-10 flex flex-col items-center">
+            <img src="./RoboschLogoWithName.svg" />
+      </div>
+    )
+}
