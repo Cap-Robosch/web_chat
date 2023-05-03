38db555309aadacfe60af7478c34e41e95a19648
viniciusventura29 <viniciusventura29@gmail.com>
2023-05-03 16:56:58 -0300

    improving index css

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
-
-const TalkArea = ({ text }: { text: string }) => {
-  if (text === "") {
-    return <div className="text-gray-700">Faça uma pergunta!</div>
-  }
-
-  const newText = text.split("\n").map((str) => <p>{str}</p>);
-  
-  return <div>{newText}</div>;
-};
-
-const LoadingDots = () => {
-  return (
-    <div className="flex">
-      <div className="h-2.5 w-2.5 bg-purple-500 rounded-full mr-1 animate-bounce"></div>
-      <div className="h-2.5 w-2.5 bg-purple-500 rounded-full mr-1 animate-bounce200"></div>
-      <div className="h-2.5 w-2.5 bg-purple-500 rounded-full animate-bounce400"></div>
-    </div>
-  )
-}
+import { Sidebar } from "./components/Sidebar";
+import { LoadingDots } from "./components/LoadingDots";
+import { TalkArea } from "./components/TalkArea";
 
 function App() {
   const [inputMessage, setInputMessage] = useState("");
   const [roboschAnswer, setRoboschAnswer] = useState("");
+  const [showMessage, setShowMessage] = useState<string[]>([""])
   const [loading, setLoading] = useState(false);
 
   const sendMessage = async () => {
     setLoading(true);
+    setShowMessage((prev) => [...prev, inputMessage])
     setInputMessage("");
     await axios
       .post("http://localhost:3000/", {
@@ -37,36 +22,48 @@ function App() {
       .then((e) => {
         setLoading(false);
         setRoboschAnswer(e.data);
+        setShowMessage((prev) => [...prev, e.data])
       });
   };
 
+  const sla=()=>{
+    console.log(showMessage)
+  }
+
   return (
-    <div className="flex flex-col gap-5 px-10 items-center justify-center h-screen w-screen">
+    <div className="flex gap-5 bg-gray-200 items-center justify-center h-screen w-screen">
+      <div className="w-80 h-full">
+        <Sidebar />
+      </div>
       {loading ? (
         <LoadingDots />
       ) : (
         <TalkArea text={roboschAnswer} />
       )}
-      <div className="flex gap-2">
-        <input
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              sendMessage();
-            }
-          }}
-          placeholder="Escreva sua mensagem aqui..."
-          className="p-2 bg-gray-200 outline-none rounded w-40 lg:w-96"
-          value={inputMessage}
-          onChange={(e) => setInputMessage(e.target.value)}
-          type="text"
-        />
-        <button
-          type="submit"
-          onClick={sendMessage}
-          className="p-2 rounded bg-gray-200 w-16 lg:w-24"
-        >
-          Enviar
-        </button>
+      <div className="flex gap-2 px-12 fixed bottom-5 right-3 lg:w-9/12">
+        <div className="relative w-full flex items-center">
+          <input
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
+            placeholder="Escreva sua mensagem aqui..."
+            className="p-3 px-4 bg-white outline-none rounded-full w-full"
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+            type="text"
+          />
+          <button
+            type="submit"
+            onClick={sendMessage}
+            className="p-2 absolute right-4 rounded-full"
+          >
+            <img className="w-6" src="./icon_send.svg" />
+          </button>
+        </div>
+        <button onClick={sla} className="w-80 bg-red-500">Ts</button>
+
       </div>
     </div>
   );
