c76e4587e73e97856f03137ce53d9d9479765c95
viniciusventura29 <viniciusventura29@gmail.com>
2023-05-04 16:53:28 -0300

    Improve css

--- src/components/LoadingDots.tsx
+++ src/components/LoadingDots.tsx
@@ -1,9 +1,9 @@
 export const LoadingDots = () => {
     return (
-      <div className="flex">
-        <div className="h-2.5 w-2.5 bg-purple-500 rounded-full mr-1 animate-bounce"></div>
-        <div className="h-2.5 w-2.5 bg-purple-500 rounded-full mr-1 animate-bounce200"></div>
-        <div className="h-2.5 w-2.5 bg-purple-500 rounded-full animate-bounce400"></div>
+      <div className="flex mt-1">
+        <div className="h-2 w-2 bg-purple-500 rounded-full mr-1 animate-bounce"></div>
+        <div className="h-2 w-2 bg-purple-500 rounded-full mr-1 animate-bounce200"></div>
+        <div className="h-2 w-2 bg-purple-500 rounded-full animate-bounce400"></div>
       </div>
     )
   }
--- src/components/TalkArea.tsx
+++ src/components/TalkArea.tsx
@@ -1,19 +1,23 @@
+import { LoadingDots } from "./LoadingDots";
+
 export const TalkArea = ({
   text,
+  loading
 }: {
+  loading: boolean,
   text: { text: string; sender: string }[];
 }) => {
   return (
     <div className={`lg:w-full h-full flex flex-col space-y-8 px-16 py-5`}>
       {text.map((msg) => (
         <p
-          className={`${
-            msg.sender === "Robosch" ? "bg-white max-w-prose" : "bg-purple-500 text-white flex self-end max-w-prose"
-          } rounded-2xl p-2`}
+          className={`${msg.sender === "Robosch" ? "bg-white max-w-prose rounded-bl-none" : "bg-purple-500 text-white flex self-end max-w-prose rounded-br-none"
+            } rounded-2xl p-2 px-3`}
         >
           {msg.text}
         </p>
       ))}
+      <div className={`${loading ? "flex" : "hidden"} p-2 bg-white rounded-2xl flex justify-center items-center rounded-bl-none w-16 `}>{loading ? <LoadingDots /> : null}</div>
     </div>
   );
 };
