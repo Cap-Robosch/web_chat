1acca399c57806ef969dcc885ca4e14b8d012f6a
Julia Helena <juliahelenam04@gmail.com>
2023-05-05 16:15:28 -0300

    sidebar finished

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,13 +4,13 @@ export const Sidebar = ()=>{
             <img src="./RoboschLogoWithName.svg" />
             <ul className="font-fonte my-7 ">
                 <li className="my-5"><a className="flex gap-x-2 items-center inline-block duration-500 hover:text-purple hover:scale-105" href="#chat">
-                    <i className="fa-regular fa-comment-dots"></i>Chat</a>
+                    <i className="fa-regular text-lg fa-comment-dots"></i>Chat</a>
                 </li>
-                <li className="my-5"><a className="flex gap-x-2 items-center inline-block duration-500 hover:text-purple hover:scale-105" href="#docs">
-                    <i className="fa-regular fa-file-lines"></i>Documentação</a>
+                <li className="my-5"><a className="flex gap-x-3 items-center inline-block duration-500 hover:text-purple hover:scale-105" href="#docs">
+                    <i className="fa-regular text-lg fa-file-lines"></i>Documentação</a>
                 </li>
                 <li className="my-5"><a className="flex gap-x-2 items-center inline-block duration-500 hover:text-purple hover:scale-105" href="#mais">
-                <i className="fa-regular fa-circle-question"></i>Saiba mais</a>
+                <i className="fa-regular text-lg fa-circle-question"></i>Saiba mais</a>
                 </li>
             </ul>
         </div>
