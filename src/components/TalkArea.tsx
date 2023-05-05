d17d7d3a5eb21512a72049f1b405649dadcd468f
viniciusventura29 <viniciusventura29@gmail.com>
2023-05-05 16:51:29 -0300

    fixing talkArea height css

--- src/components/TalkArea.tsx
+++ src/components/TalkArea.tsx
@@ -2,22 +2,31 @@ import { LoadingDots } from "./LoadingDots";
 
 export const TalkArea = ({
   text,
-  loading
+  loading,
 }: {
-  loading: boolean,
+  loading: boolean;
   text: { text: string; sender: string }[];
 }) => {
   return (
-    <div className={`lg:w-full h-full flex flex-col space-y-8 px-16 py-5`}>
+    <div className="lg:w-full flex flex-col space-y-8 px-16 pt-6 pb-6 overflow-y-scroll h-[90vh]" id="messageWindow">
       {text.map((msg) => (
         <p
-          className={`${msg.sender === "Robosch" ? "bg-white max-w-prose rounded-bl-none" : "bg-purple-500 text-white flex self-end max-w-prose rounded-br-none"
-            } rounded-2xl p-2 px-3`}
+          className={`${
+            msg.sender === "Robosch"
+              ? "bg-white max-w-prose rounded-bl-none"
+              : "bg-purple-500 text-white flex self-end max-w-prose rounded-br-none"
+          } rounded-2xl p-2 px-3 whitespace-pre-line text-sm 2xl:text-base`}
         >
           {msg.text}
         </p>
       ))}
-      <div className={`${loading ? "flex" : "hidden"} p-2 bg-white rounded-2xl flex justify-center items-center rounded-bl-none w-16 `}>{loading ? <LoadingDots /> : null}</div>
+      <div
+        className={`${
+          loading ? "flex" : "hidden"
+        } p-2 bg-white rounded-2xl flex justify-center items-center rounded-bl-none w-16 `}
+      >
+        {loading ? <LoadingDots /> : null}
+      </div>
     </div>
   );
 };
