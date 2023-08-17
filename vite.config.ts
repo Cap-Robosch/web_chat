5ef14439c62cfa18bbbb293f5ca9fed6a48e3952
viniciusventura29 <viniciusventura29@gmail.com>
2023-08-17 15:41:12 -0300

    new robosch pwa icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    VitePWA({ registerType: 'autoUpdate' })],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.svg"],
+      manifest: {
+        name: "Robosch",
+        short_name: "Robosch",
+        description: "Robosch chatbot",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+          {
+            src: "logo.svg",
+            sizes: "512x512",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
+});
